27b558e7610f0979a6aeff252118a41b06696196
甜虾 <daizhen.dz@alibaba-inc.com>
2017-06-23 23:27:44 +0800

    add

--- src/pages/Gradient.vue
+++ src/pages/Gradient.vue
@@ -5,16 +5,17 @@
         </div>
         <div class="control">
             <div v-for="i in contorlNum">
-                <input type="range" @change="onRangeChange($event, i)" min="0" max="100" step="1">
                 <input type="color" @change="onColorChange($event, i)">
-                <span @click="add" v-if="i == 1">+</span>
+                <input type="range" @change="onRangeChange($event, i)" min="0" max="100" step="1">
+                <button @click="add" v-if="i == 1">+</button>
             </div>
         </div>
         <div class="code">
             <pre>
 &lt;div&gt;&lt;/div&gt;
-
-{{gradientStyle}}
+{
+    background-image: {{gradientStyle['background-image']}}
+}
             </pre>
         </div>
     </div>
@@ -26,20 +27,16 @@ export default {
         return {
             contorlNum: 2,
             gradientStyle: {
-                'background-image': 'linear-gradient(#fb3 50%, #58a 0)'
+                'background-image': 'linear-gradient(#000 50%, #000 0)'
             }
         }
-    },
-
-    computed: {
-
     },
 
     methods: {
         createVal () {
             let i = 1;
             while (i <= this.contorlNum) {
-                this['rangeVal' + i] = this['rangeVal' + i] || 0;
+                this['rangeVal' + i] = this['rangeVal' + i] || 50;
                 this['colorVal' + i] = this['colorVal' + i] || '#000';
                 i ++;
             }
@@ -48,6 +45,7 @@ export default {
         add () {
             this.contorlNum ++;
             this.createVal();
+            this.createStyle();
         },
 
         onRangeChange ($event, index) {
@@ -94,6 +92,7 @@ export default {
         height: 300px;
         border: 1px solid #ccc;
         margin-right: 10px;
+        box-sizing: border-box;
     }
     .result {
         position: relative;
@@ -105,14 +104,31 @@ export default {
         left: 50%;
         top: 50%;
         transform: translate(-50%, -50%);
-        background-color: #eee;
+        background-color: #ccc;
     }
     .control {
         overflow: auto;
+        padding: 10px;
+    }
+    .control div {
+        margin-top: 10px;
+    }
+    .control button {
+        width: 20px;
+        height: 20px;
+        text-align: center;
+        line-height: 20px;
+        font-size: 20px;
+        cursor: pointer;
     }
     .code pre {
         background-color: #ccc;
-        width: 500px;
+        width: 695px;
         padding: 10px;
+        white-space: pre-wrap;
+    }
+    input[type=range] {
+        width: 250px;
+        cursor: pointer;
     }
 </style>
